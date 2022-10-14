4874cfcf538a46e886cc6afbb6afce1be1b2553a
goonerlabs <goonerlabs@gmail.com>
2022-10-14 04:08:06 -0700

    complete task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char reuslt[] = "Programming is like building a multilingual puzzle";
+	char result[] = "Programming is like building a multilingual puzzle";
 
 	puts(result);
 
