b3ddad7071a82cfd60ba59f41e50e21032dfb8b6
goonerlabs <goonerlabs@gmail.com>
2022-10-14 04:25:47 -0700

    complete task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- *
- * main - prints the phrase Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints phrase to standard output
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
