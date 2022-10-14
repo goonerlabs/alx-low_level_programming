5f5d37783e56203ef852da4bd5c367c9f907f2c2
goonerlabs <goonerlabs@gmail.com>
2022-10-14 04:28:46 -0700

    complete task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - prints phrase to standard output
+ * it returns 0 if succesful and another numebr otherwise
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
