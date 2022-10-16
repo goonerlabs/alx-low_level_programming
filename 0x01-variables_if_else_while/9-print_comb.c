98c6bb76f431b19f1714fc2065e906a06b520bd9
goonerlabs <goonerlabs@gmail.com>
2022-10-15 18:45:29 -0700

    complete task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	while (n <= 57)
 	{
 		putchar((char) n);
-		putchar(',');
-		putchar(' ');
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
 	putchar('\n');
