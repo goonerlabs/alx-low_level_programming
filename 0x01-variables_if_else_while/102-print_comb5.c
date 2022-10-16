9f59df552cd80db1e9a8590799181991c4c38118
goonerlabs <goonerlabs@gmail.com>
2022-10-15 21:30:46 -0700

    complete task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - prints all combinations of base 10 integers
+ *betwwen 0 and 99 followed by a newline
+ * Return: 0 if succesful and another number otherwise
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (j <= i)
+			{
+				continue;
+			}
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
