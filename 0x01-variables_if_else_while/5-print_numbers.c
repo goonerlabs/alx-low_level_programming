dacd8c809148d1d76e255a5d0a4909d3d599a976
goonerlabs <goonerlabs@gmail.com>
2022-10-15 17:30:22 -0700

    complete task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - prints all base 10 numbers followed by a newline
+ * Return: 0 if succesful and another number otherwise
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; 1++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
