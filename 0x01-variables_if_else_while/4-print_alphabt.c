b36c75463a8c3736e0b7fb081c98e9b177843224
goonerlabs <goonerlabs@gmail.com>
2022-10-15 17:13:56 -0700

    complete task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabets excluding q and e
+ * followed by a newline
+ * Return: 0 if succesful and another number otherwise
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		if (n == 'q' || n == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
