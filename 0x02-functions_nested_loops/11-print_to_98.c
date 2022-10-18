c0d5fc61f92874874ff28862accb990f7d486f88
goonerlabs <goonerlabs@gmail.com>
2022-10-18 14:31:49 -0700

    complete task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - prints numbers till 98
+ * Description: prints consecutive real numbers from n to 98
+ * @n: parameter of type int
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar('0' + i);
+}
