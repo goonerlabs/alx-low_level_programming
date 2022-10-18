f57cf9a000ca2510d29b28cb74977de06309a892
goonerlabs <goonerlabs@gmail.com>
2022-10-18 16:10:43 -0700

    complete task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,13 +8,14 @@
  */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-	{
-		return (void);
-	}
 	int i;
 	int j;
 	int k;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
 
 	for (i = 0; i <= n; i++)
 	{
