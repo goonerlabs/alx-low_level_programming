35994d4f4c850d39e50e46f731458a8d0b6044d6
goonerlabs <goonerlabs@gmail.com>
2022-10-18 16:04:30 -0700

    complete task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table
+ * Description: prints the nth times table
+ * @n: parameter of type int
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0' + k);
+			}
+
+			else if ((j != 0) && k <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+
+			else if (k >= (n + 1))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (k / 10));
+				_putchar('0' + (k % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
