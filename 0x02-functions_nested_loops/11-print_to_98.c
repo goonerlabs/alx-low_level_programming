03ff82e5fde830b1b3502128ce2def7fb24aa71e
goonerlabs <goonerlabs@gmail.com>
2022-10-18 15:44:04 -0700

    complete task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
-		{	
+		{
 			if (i == 98)
 			{
 				printf("%d\n", i);
@@ -28,7 +28,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else 
+	else
 	{
 		for (j = n; j >= 98; j--)
 		{
