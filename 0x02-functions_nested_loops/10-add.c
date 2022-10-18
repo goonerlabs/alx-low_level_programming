2e931996ec0f5a6707273a15bb70bda8e83847d4
goonerlabs <goonerlabs@gmail.com>
2022-10-18 14:24:22 -0700

    complete task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * Description: returns the sum of x and y
+ * @x: parameter of type int
+ * @y: parameter of type int
+ * Return: x + y
+ */
+int add(int x, int y)
+{
+	int i;
+	int j;
+
+	return (x + y);
+}
