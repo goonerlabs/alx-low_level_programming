3ee20b651fa97596ae38145fef65e10f072825e0
goonerlabs <goonerlabs@gmail.com>
2022-10-18 14:00:09 -0700

    complete task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	while (i < 10 )
+	{
+		while ( j < 10)
+		{
+			k = i * j;
+			
+			if (j == 0)
+			{
+				_putchar('0' + k);
+			}
+			
+			else if (j != 0 && k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' (k / 10));
+				_putchar('0' (k % 10));
+			}
+			j++;
+			_putchar('\n');
+		}
+		i++;
+	}
+}
