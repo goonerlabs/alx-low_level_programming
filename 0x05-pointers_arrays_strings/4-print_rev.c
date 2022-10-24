d08786ed385f3e346e415d32ab9cebe8d14ce592
goonerlabs <goonerlabs@gmail.com>
2022-10-24 16:41:14 -0700

    complete task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order to stdout
+ *
+ * @s: input string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	while (i > 0)
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	_putchar('\n');
+}
