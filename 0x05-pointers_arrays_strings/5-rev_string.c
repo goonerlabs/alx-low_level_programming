2508151176abe98ba4a5224f2c804b08125648ca
goonerlabs <goonerlabs@gmail.com>
2022-10-24 17:23:58 -0700

    complete task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: input string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+
+	i = strlen(s) - 1;
+
+	while (s[j] != '\0')
+	{
+		s[j] = s[i];
+		j++;
+		i--;
+	}
+}
