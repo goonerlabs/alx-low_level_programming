ceb36499358a7df9de1ebd5b70ea057aacebb382
goonerlabs <goonerlabs@gmail.com>
2022-10-24 17:38:34 -0700

    complete task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,20 @@ void rev_string(char *s)
 {
 	int i;
 	int j;
+	char tmp[];
 
 	i = strlen(s) - 1;
 	j = 0;
 
-	while (s[i] <= 0)
+	while (i <= 0)
 	{
-		*(s + j) = s[i];
+		tmp[j] = s[i];
 		j++;
 		i--;
+	}
+
+	while (j <= 0 )
+	{
+		s[i] = j[i];
 	}
 }
