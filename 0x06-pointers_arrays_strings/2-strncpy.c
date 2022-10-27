7eae0cabf9ceb560033389d67a7673feeed0dc35
goonerlabs <goonerlabs@gmail.com>
2022-10-27 09:16:05 -0700

    complete task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * _strncpy - copies a string 
+ * _strncpy - copies a string
  *
  * @dest: destination string
  * @src: source string
@@ -16,9 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; (i < n && i < (int) strlen(src)); i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-	
+
 	while (i < n)
 	{
 		dest[i] = '\0';
