46497ecfae2427e7feaaab7fcf437a9b4a01d347
goonerlabs <goonerlabs@gmail.com>
2022-10-27 21:44:09 -0700

    complete task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,33 +10,24 @@
 char *cap_string(char *str)
 {
 
-	int i;
+	int i, j;
+
+	char skip[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; i < (int) strlen(str); i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
-			continue;
-		}
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ','
-				|| str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?'
-				|| str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{'
-				|| str[i] == '}')
-		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
-			}
+
+			for (j = 0; j < (int) strlen(skip); j++)
+				if (str[i] == skip[j])
+				{
+					if (str[1 + i] >= 'a' && str[1 + i] <= 'z')
+					{
+						str[1 + i] = str[1 + i] - 32;
+					}
+				}
 		}
 	}
 	return (str);
