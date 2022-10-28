3295d0de98b59e8622113f7b451943ee6a297e45
goonerlabs <goonerlabs@gmail.com>
2022-10-27 21:55:15 -0700

    complete task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,14 @@ char *cap_string(char *str)
 
 	char skip[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; i < (int) strlen(str); i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 
-			for (j = 0; j < (int) strlen(skip); j++)
+			for (j = 0; skip[j] != '\0'; j++)
+			{
 				if (str[i] == skip[j])
 				{
 					if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
@@ -28,6 +29,7 @@ char *cap_string(char *str)
 						str[i + 1] = str[i + 1] - 32;
 					}
 				}
+			}
 		}
 	}
 	return (str);
