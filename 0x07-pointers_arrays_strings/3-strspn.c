a7f70596078fe376050142f16654836e76fb2534
goonerlabs <goonerlabs@gmail.com>
2022-11-04 12:55:01 -0700

    complete task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdbool.h>
 
 /**
  * _strspn - returns the number of bytes
@@ -16,15 +15,15 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		bool found;
+		int  found;
 
-		found = false;
+		found = 0;
 
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				found = true;
+				found = 1;
 				break;
 			}
 			if (!found)
