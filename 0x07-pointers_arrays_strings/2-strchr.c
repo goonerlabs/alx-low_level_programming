4ef6e57a585b6fb4f31608ebfa4f402be68a99e8
goonerlabs <goonerlabs@gmail.com>
2022-11-04 10:40:00 -0700

    complete task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * strchr - returns a pointer to the first occurence of a character * in a string
+ * strchr - returns a pointer to the first 
+ * occurence of a character in a string
  * @s: the string to check
  * @c: the character to check for
  * Return: a pointer to the character or NULL
  */
 char *_strchr(char *s, char c)
 {
-	while(*s != c && *s != '\0')
+	while (*s != c && *s != '\0')
 	{
 		s++;
 	}
