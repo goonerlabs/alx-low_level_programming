8af8ed4e75ba1a8fae01fa7a072ff39bb2f843b8
goonerlabs <goonerlabs@gmail.com>
2022-11-06 21:18:51 -0800

    complete task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the swuare root of n
+ * @n: integer input
+ * Return: square root of n  or -1 if n is negative
+ */
+int _sqrt(int n, int min, int max);
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else 
+	{
+		return (_sqrt(n, 1, n));
+	}
+
+}
+
+/**
+ * _sqrt - returns the sqare root of a number
+ * @n: integer input
+ * @min: min guess
+ * @max: max guess
+ * Return: square root of n
+ */
+int _sqrt(int n, int min, int max)
+{
+	int guess, guess_squared;
+
+	guess = (min + max) / 2;
+	guess_squared = guess * guess;
+
+	if (guess_squared == n) 
+	{
+		return (guess);
+	}
+	else if (guess_squared < n)
+	{
+		_sqrt(n, guess + 1, max);
+	}
+	else
+	{
+		_sqrt(n, min, guess -1)
+	}
+}
+
