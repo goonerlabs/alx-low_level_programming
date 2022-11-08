1347b57820a5f7f8b1781d92d9f5346e205258bb
goonerlabs <goonerlabs@gmail.com>
2022-11-07 21:16:22 -0800

    complete task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the multiplication of
+ * command line arguments
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: 0 if succesful 1 otherwise
+ */
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (agrc != 3)
+	{
+		return (1);
+	}
+	else
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+		return (0);
+	}
+}
