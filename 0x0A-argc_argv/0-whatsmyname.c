bc192f668268367f8ba6455fec683e5155ba0d85
goonerlabs <goonerlabs@gmail.com>
2022-11-07 20:56:04 -0800

    complete task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,8 @@
  * @argv: the argument vector
  * Return: 0 if succesful 1 otherwise
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
