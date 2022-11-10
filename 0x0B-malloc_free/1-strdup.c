80af0e03edcf0ced57829f37169b8820a5770b2f
goonerlabs <goonerlabs@gmail.com>
2022-11-10 02:43:35 -0800

    complete task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: a pointer to the input string
+ * Return: a pointer to the duplicated string
+ * or NULL if unsuccesful
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+
+	if (strlen(str) == 0 || str == NULL)
+	{
+		return (NULL);
+	}
+	
+	ptr = (char *) malloc(strlen(str) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+
+}
