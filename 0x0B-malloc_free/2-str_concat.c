ebefedc9809a0c0685c1faca293d8be28ef037f8
goonerlabs <goonerlabs@gmail.com>
2022-11-10 10:44:50 -0800

    complete task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,22 +20,22 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	if (s1 == NULL)
+	
+	if (s1)
 	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
+
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			ptr[i] = s1[i];
+		}
 	}
 
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s2)
 	{
-		ptr[i] = s1[i];
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		ptr[i + j] = s2[j];
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			ptr[i + j] = s2[j];
+		}
 	}
 	ptr[i + j] = '\0';
 	return (ptr);
