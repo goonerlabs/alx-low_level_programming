87ab2d389b8a1c978d86387635cd8ef90086484b
goonerlabs <goonerlabs@gmail.com>
2022-11-14 11:51:44 -0800

    complete task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all aguments of argv[]
+ * @ac: argument count
+ * @av: double pointer to argV[]
+ * Return: a pointer to the new string or NULL on fail
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, l;
+	char *ptr;
+
+	i = 0;
+	k = 0;
+	l = 0;
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			l++;
+			j++;
+		}
+		i++;
+		l++;
+	}
+	ptr = (char *) malloc(l * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			ptr[k] = av[i][j];
+			k++;
+		}
+		ptr[k] = '\n';
+		k++;
+	}
+	ptr[k] = '\n';
+}
+
+
+
