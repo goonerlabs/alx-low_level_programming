85d4aa464409119dca8040ee09f1971d4c337983
goonerlabs <goonerlabs@gmail.com>
2022-11-15 09:08:47 -0800

    complete task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,27 +18,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
 	}
-
 	new_ptr = malloc(new_size);
-
 	if (new_ptr == NULL)
 	{
 		return (NULL);
 	}
-
 	old_ptr = ptr;
-
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
@@ -46,7 +40,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = old_ptr[i];
 		}
 	}
-
 	if (old_size > new_size)
 	{
 		for (i = 0; i < new_size; i++)
@@ -54,7 +47,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = old_ptr[i];
 		}
 	}
-
 	free(ptr);
 	return (new_ptr);
 }
