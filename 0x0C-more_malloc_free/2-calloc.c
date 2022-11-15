c0fad6f4995ea2629cd4eb7245b8d78e110fe6f8
goonerlabs <goonerlabs@gmail.com>
2022-11-15 07:35:11 -0800

    complete task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates a block of memory for an array
+ * @nmemb: number of elements in the array
+ * @size: the size of each element of the array
+ * Return: a poter to the array or null on fail
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
+}
