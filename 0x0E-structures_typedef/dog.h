1bd846c990b0284ed152ddc3da4d6b2fdf359ace
goonerlabs <goonerlabs@gmail.com>
2022-11-16 10:00:51 -0800

    complete task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a structure of type dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog owner
+ */
+
+struct Dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
