df603b4c243e68635059fb1645bf9c98b3a33edf
goonerlabs <goonerlabs@gmail.com>
2022-11-16 10:25:40 -0800

    complete task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,29 @@
-/**
- * struct dog - a structure of type dog
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the name of the dog owner
- */
+#ifndef DOG_H
+#define DOG_H
 
-struct dog 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
