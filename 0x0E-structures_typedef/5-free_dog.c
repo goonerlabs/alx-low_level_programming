63312fa74c98cc4b73089ac392912511c5ed3f1e
goonerlabs <goonerlabs@gmail.com>
2022-11-17 00:28:10 -0800

    complete task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees up memory allocated to the struct dog
+ * @d: pointer to the struct 
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
