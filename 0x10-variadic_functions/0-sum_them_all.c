e5e7c9abf185383bfb16da14efa445d414a78600
goonerlabs <goonerlabs@gmail.com>
2022-11-17 11:40:39 -0800

    complete task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,19 +7,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-	{
-		return (0);
-	}
-
+	unsigned int i;
+	int sum;
+	
+	sum = 0;
+	
 	va_list ptr;
 
 	va_start(ptr, n);
-
-	unsigned int i;
-	int sum;
-
-	sum = 0;
 
 	for (i = 0; i < n; i++)
 	{
