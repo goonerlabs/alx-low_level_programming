de787dfdd90ce373d1ebdc5a6155ee19964c9b2e
goonerlabs <goonerlabs@gmail.com>
2022-11-16 23:56:44 -0800

    complete task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,6 +17,6 @@ void print_dog(struct dog *d)
 	d->name = (d->name == NULL) ? "(nil)" : d->name;
 	d->owner = (d->owner == NULL) ? "(nil)" : d->owner;
 
-	
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
