93667b48c4861006dbd0ff93eae823218e850408
goonerlabs <goonerlabs@gmail.com>
2022-11-17 13:15:04 -0800

    complete task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		for (i = 0; i < n; i++)
 		{
-			tmp = va_arg(list, char*) ? va_arg(list, char*) : "(ni)l";
+			tmp = va_arg(list, char*) ? va_arg(list, char*) : "(nil)";
 
 			if (!separator)
 			{
