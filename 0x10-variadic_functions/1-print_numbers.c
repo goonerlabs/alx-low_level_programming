f67788a3dc58d439582d705afbae173cf10711e4
goonerlabs <goonerlabs@gmail.com>
2022-11-17 12:14:50 -0800

    complete task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ * print_numbers - prints the numbers passed to it as arguments
+ * @separator: the separator to be printed in between the numbers
+ * @n: the argument count
+ * Return: void
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (separator != NULL)
+	{
+
+		va_start(list, n);
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(list, int));
+		
+			if (i < (n - 1))
+			{
+				printf("%s", separator);
+			}
+		}
+		printf("\n");
+	}
+
+		va_end(list);
+}
