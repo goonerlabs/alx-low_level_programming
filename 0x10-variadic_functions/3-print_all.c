ef07e02f20346c709e51185703f5dc36adfa355b
goonerlabs <goonerlabs@gmail.com>
2022-11-18 02:31:44 -0800

    complete task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,21 +21,21 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", sep, va_arg(list, char));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f", sep, va_arg(list, float));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
-					str = va_arg(list, char*);
-					if (!str)
+					tmp = va_arg(list, char*);
+					if (!tmp)
 					{
-						str = "(nil)";
+						tmp = "(nil)";
 					}
-					printf("%s%s", sep, str);
+					printf("%s%s", sep, tmp);
 					break;
 				default:
 					i++;
