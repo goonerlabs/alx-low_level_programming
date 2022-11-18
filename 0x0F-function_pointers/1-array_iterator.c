187ea1e293769d526075d018718a465dbafcfbd0
goonerlabs <goonerlabs@gmail.com>
2022-11-18 10:00:10 -0800

    complete task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes the function passed as an argument 
+ * on each element of an array passed as an argument
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !size || !action)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
