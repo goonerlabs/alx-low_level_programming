8b421017fa0994589f847d164da093c486ab4b23
goonerlabs <goonerlabs@gmail.com>
2022-11-18 10:13:07 -0800

    complete task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches the array passed as an argument for
+ * any element for which the function cmp does not return 0
+ * @array: the array
+ * @size: the size of the array
+ * @cmp: the function pointer
+ * Return: the element if found else -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (!array || !size || !cmp)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0))
+		{
+			return (array[i]);
+		}
+	}
+	return (-1);
+}
