5cb1f21d29ffdcc1cb77bb1c78d1c893de788fde
goonerlabs <goonerlabs@gmail.com>
2022-11-30 04:45:56 -0800

    complete task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,21 @@
  */
 size_t print_list(const list_t *h)
 {
-	list_t **temp;
 	size_t n;
 
-	*temp = *h;
 	n = 0;
 
-	while (temp->next != NULL)
+	while (h)
 	{
-		printf("[%u] %s\n", temp->len, temp->str);
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", temp->len, temp->str);
+		}
+
 		temp = temp->next;
 		n++;
 	}
