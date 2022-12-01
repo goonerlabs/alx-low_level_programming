3119f9cdb1968bfa3c75322e0d29c242adb51b0a
goonerlabs <goonerlabs@gmail.com>
2022-11-30 20:27:03 -0800

    complete task

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 /**
- * list_len - returns the number of element
+ * listint_len - returns the number of elements
  *
  * @h: a pointer to the head node
  * Return: the number of nodes
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t n;
+	size_t len;
 
-	n = 0;
+	len = 0;
 
 	while (h)
 	{
 		h = h->next;
-		n++;
+		len++;
 	}
-	return (n);
+	return (len);
 }
 
