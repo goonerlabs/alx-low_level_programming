0f829e3b240ae60834606b99522bda587a703782
goonerlabs <goonerlabs@gmail.com>
2022-11-30 20:54:08 -0800

    complete task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * free_list - frees up memory allocated to
+ * free_listint - frees up memory allocated to
  * a linked list
  * @head: the head node of the list
  * Return: void
  */
 
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
-	list_t *temp;
+	listint_t *temp;
 
 	while (head)
 	{
 		temp = head->next;
-		free(head->str);
 		free(head);
 		head = temp;
 	}
