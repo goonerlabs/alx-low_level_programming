2f25f19a2b570384dc9d0724f615cbb9b3a15cb2
goonerlabs <goonerlabs@gmail.com>
2022-11-30 23:27:50 -0800

    complete task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return(new_node);
+		return (new_node);
 	}
 
 	while (temp && (i < idx))
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next  = temp->next;
 			temp->next = new_node;
-			return(new_node);
+			return (new_node);
 		}
 		temp = temp->next;
 		i++;
