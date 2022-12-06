2b8efdb7ffa2b97b781e249a777fc2dc3bd2b013
goonerlabs <goonerlabs@gmail.com>
2022-12-06 12:04:24 -0800

    complete task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * binary_to_uint - converts a binary number to a uint
@@ -11,29 +10,26 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result, exp;
-	int i, j, len;
+	int len;
 
-	len = strlen(b);
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	for (i = 0; b[i]; i++)
+	for (len = 0; b[i]; len++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 	}
 
-	result = 0;
-	exp = 1;
 
-	for (j = (len - 1); j >= 0; j--, exp *= 2)
+	for (result = 0, exp = 1, len--; len >= 0; len--, exp *= 2)
 	{
-		if (b[j] == '1')
+		if (b[len] == '1')
 		{
 			result += exp;
 		}
@@ -42,5 +38,3 @@ unsigned int binary_to_uint(const char *b)
 
 	return (result);
 }
-
-
