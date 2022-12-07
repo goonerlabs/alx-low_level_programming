2d2970b99e44a606d1fef5b1bab9f0ff7ef80fea
goonerlabs <goonerlabs@gmail.com>
2022-12-07 02:07:33 -0800

    complete task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * flip_bits - checks how many bits we have to filp
+ * to change n to m
+ * @n: first input int
+ * @m: second input int
+ * Return: the number of bits needed to be flipped
+ * (success)
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int tmp;
+	unsigned int res;
+
+	tmp = n ^ m;
+
+	while (tmp)
+	{
+		if (tmp & 1)
+		{
+			res++;
+		}
+		tmp = (tmp >> 1);
+	}
+	return (res);
+}
