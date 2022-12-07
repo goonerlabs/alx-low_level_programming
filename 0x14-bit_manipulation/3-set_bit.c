19803837a4815388dae7c21e3a71bee6e2f1e45b
goonerlabs <goonerlabs@gmail.com>
2022-12-07 01:29:37 -0800

    complete task

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - setsthe bit of n at index
+ * @n: the number whose bit we want to set
+ * @: the index of n at which the bit would be set
+ * Return: 1 (success) -1 (failure)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+	{
+		return (-1);
+	}
+	
+	return (*n = *n | (1 << index));
+}
