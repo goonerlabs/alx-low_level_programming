676a441558650b282c04ca5b7decdad7e414a9ab
goonerlabs <goonerlabs@gmail.com>
2022-12-07 02:40:30 -0800

    complete task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianess of a number 
+ *
+ * Returns: 0 if big endian and 1 otherwise
+ */
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c;
+
+	i = 1;
+
+	c = (char *) &i;
+
+	if (*c)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
