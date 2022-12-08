24b441313fad91932bd94a6fae6931cc531d350b
goonerlabs <goonerlabs@gmail.com>
2022-12-08 04:28:57 -0800

    complete task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	w = write(o, text_content, len);
 
