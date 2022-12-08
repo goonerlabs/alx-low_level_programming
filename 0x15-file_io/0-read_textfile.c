cb25f33ceee4f29127f3a57ab1ba40d0ed4d7112
goonerlabs <goonerlabs@gmail.com>
2022-12-08 03:59:45 -0800

    complete task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: a pointer to the file to read
+ * @letters: the number of characters to read from the file
+ * Return: the number of characters read and written or
+ * 0 if the pointer to the file is NULL or write fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(letters * sizeof(char));
+
+	if (buff == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+
+	r = read(o, buff, letters);
+
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (o == -1 || r == -1 || w == -1 || r != w)
+		return (0);
+
+	free(buff);
+	close(o);
+
+	return (w);
+}
