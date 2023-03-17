a87d85901f2f70b686bb6eca6adc4d58c197776b
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-17 06:11:29 -0700

    complete task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+	
+	c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	
+	putchar('\n');
+	
+	return (0);
+}
+
