003a5aca89dbe21c4a762217b8620f8e177fa46f
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-17 06:05:32 -0700

    complete task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 
 	if (res > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, res);
+		printf("Last digit of %d is %d and is greater than 5\n", n, res);
 	}
 	else if (n == 0)
 	{
