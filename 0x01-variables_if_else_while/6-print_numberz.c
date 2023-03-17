7b4172c05d74285855d807bba81f8c8f6180f108
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-17 09:11:37 -0700

    complete task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 60; n <= 71; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
+
