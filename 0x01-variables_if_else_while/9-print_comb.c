993a08dc8aa622681ea14bee9454c93beb893976
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-17 09:36:45 -0700

    complete task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 57)
+		{
+			putchar(n);
+			putchar(', ');
+		}
+
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
