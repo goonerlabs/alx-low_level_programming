bac3191486ee3fb89d8e81bdd4b5a143701bfe7b
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-17 10:26:32 -0700

    complete task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			
+			if (i != 56 || j != 57)
+			{	
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
