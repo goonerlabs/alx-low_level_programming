41b98048c99b438e405e9ff42be9da647a580df6
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-21 13:32:41 -0700

    complete task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the sum of all even fibonacci numbbers up to 4,000,000
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int i, j, k, temp, sum;
+
+	j = 1;
+
+	k = 2;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+		{
+			sum += j;
+		}
+
+
+		temp = j + k;
+		j = k;
+		k = temp;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
