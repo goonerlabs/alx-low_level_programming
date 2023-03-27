20fe938a3b199cc41dbf2a62bb1944374c4fffad
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-27 14:46:17 -0700

    complete task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,21 @@
 
 int _atoi(char *s)
 {
-	int i = 0;
-	int num = 0;
 	int sign = 1;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			num = num * 10 + (s[i] - '0');
-			i++;
-		}
-		else if (s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
-		{
-			sign = -1;
-			i++;
-		}
-		else
-		{
-			i++;
-		}
-	}
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
 	return (num * sign);
 }
+
