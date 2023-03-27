141a7ed5e69043a3bc1c3f516775fd172b59b5ff
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-27 12:49:16 -0700

    complete task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	j = i / 2;
+	while (str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
