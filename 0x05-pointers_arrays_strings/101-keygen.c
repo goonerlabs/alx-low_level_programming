518d6534f3c344467a28b2e7779ff42c6fd3b847
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-29 01:45:55 -0700

    complete task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,36 +3,29 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
- * Description: This program generates random characters 
- * to crack the crackme password
+ * Description: This function generates a random keygen.
  *
- * Return: Always 0 (Success)
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
+	int k = 0;
 	time_t t;
-	
 
 	srand((unsigned int) time(&t));
-
 	while (j < 2772)
 	{
 		i = rand() % 128;
-
 		if ((j + i) > 2772)
-		{
 			break;
-		}
-		else
-		{
-			printf("%c", i);
-			j += i;
-		}
+		j += i;
+		k++;
+		printf("%c", i);
 	}
 	printf("%c\n", (2772 - j));
 	return (0);
