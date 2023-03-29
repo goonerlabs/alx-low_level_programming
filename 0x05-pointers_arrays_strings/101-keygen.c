696ea91261b4594a30b71ab0135c70e70fa96202
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-29 01:49:02 -0700

    complete task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,7 +14,6 @@ int main(void)
 {
 	int i = 0;
 	int j = 0;
-	int k = 0;
 	time_t t;
 
 	srand((unsigned int) time(&t));
@@ -24,9 +23,7 @@ int main(void)
 		if ((j + i) > 2772)
 			break;
 		j += i;
-		k++;
 		printf("%c", i);
 	}
-	printf("%c\n", (2772 - j));
 	return (0);
 }
