63d9cfe5ec047a99bfa9f7251c853151a1d37346
goonerlabs <aowolabiadewumi@yahoo.com>
2023-03-30 22:08:47 -0700

    complete task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if strings are equal, less than 0 if s1 < s2, 
+ * Return: 0 if strings are equal, less than 0 if s1 < s2,
  * greater than 0 if s1 > s2
  */
 
