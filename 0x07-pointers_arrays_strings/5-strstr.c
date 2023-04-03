ccbf406ecd1889463f7483ecb7f33fde91f22dc4
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-03 13:53:08 -0700

    complete task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,20 +11,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i, j, flag;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
+			flag = 0;
+			
 			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (haystack[i + j] != needle[j])
 				{
+					flag = 1;
 					break;
 				}
 			}
-			if (needle[j] == '\0')
+			if (flag == 0)
 			{
 				return (haystack + i);
 			}
