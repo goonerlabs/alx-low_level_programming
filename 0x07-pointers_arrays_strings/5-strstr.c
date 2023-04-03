a57e5e4b22e37a2e2a953eb8b25ae08d3a9d5726
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-03 13:48:52 -0700

    complete task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to be searched
+ * @needle: substring to be searched
+ *
+ * Return: pointer to the beginning of the substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+	}
+	return (NULL);
+}
