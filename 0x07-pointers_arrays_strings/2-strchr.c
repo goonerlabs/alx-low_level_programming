64a92b4a165656b12498274c7e5474e4880fc347
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-03 12:47:19 -0700

    complete task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,15 +13,15 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+
+	while (1)
 	{
 		if (s[i] == c)
-		{
-			p = &s[i];
-			return (p);
-		}
+			return (s + i);
+		if (s[i] == '\0')
+			return (NULL);
+		i++;
 	}
-	return (NULL);
 }
