a1aa1a13067cd1ba96d2100a209be981961225a7
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-11 03:48:04 -0700

    complete task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **arg)
+{
+	int i, j, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	result = i * j;
+	printf("%d\n", result);
+	return (0);
+}
