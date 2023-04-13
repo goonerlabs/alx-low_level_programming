7a165445c8d165bfc7541f4ab3b24d00ccdaa666
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-13 05:10:52 -0700

    complete task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	void *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -24,5 +25,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
 
-	return (ptr);
+	p = (void *) ptr;
+	return (p);
 }
