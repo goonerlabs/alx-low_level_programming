98650a1dd61a722c2d17982bcd930273f280209c
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-13 06:44:40 -0700

    complete task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,9 +1,20 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #define ERROR_MSG "Error\n"
+
+/**
+ * error_exit - prints error message and exits
+ */
+
+void error_exit(void)
+{
+	printf("%s", ERROR_MSG);
+	exit(98);
+}
 
 /**
  * main - multiplies two numbers
@@ -14,53 +25,44 @@
 
 int main(int argc, char *argv[])
 {
-    char *num1, *num2;
-    int len1, len2, len, i, carry, digit1, digit2, has_nonzero_digit;
-	int *result;
+	char *num1, *num2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, x = 0;
 
-	has_nonzero_digit = 0;
-
-    if (argc != 3) {
-        printf("%s", ERROR_MSG);
-        return 1;
-    }
-    num1 = argv[1], num2 = argv[2];
-    len1 = strlen(num1), len2 = strlen(num2);
-    if (!isdigit(*num1) || !isdigit(*num2)) {
-        printf("%s", ERROR_MSG);
-        return 98;
-    }
-    len = len1 + len2 + 1;
-    result = malloc(sizeof(int) * len);
-    if (!result)
-        return 1;
-    for (i = 0; i <= len1 + len2; i++)
-        result[i] = 0;
-    for (len1 = len1 - 1; len1 >= 0; len1--)
-    {
-        digit1 = num1[len1] - '0';
-        carry = 0;
-        for (len2 = strlen(num2) - 1; len2 >= 0; len2--)
-        {
-            digit2 = num2[len2] - '0';
-            carry += result[len1 + len2 + 1] + (digit1 * digit2);
-            result[len1 + len2 + 1] = carry % 10;
-            carry /= 10;
-        }
-        if (carry > 0)
-            result[len1 + len2 + 1] += carry;
-    }
-    for (i = 0; i < len - 1; i++)
-    {
-        if (result[i])
-            has_nonzero_digit = 1;
-        if (has_nonzero_digit)
-            putchar(result[i] + '0');
-    }
-    if (!has_nonzero_digit)
-        putchar('0');
-    putchar('\n');
-    free(result);
-    return 0;
+	if (argc != 3)
+		error_exit();
+	num1 = argv[1], num2 = argv[2];
+	len1 = strlen(num1), len2 = strlen(num2);
+	if (!isdigit(*num1) || !isdigit(*num2))
+		error_exit();
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = num1[len1] - '0';
+		carry = 0;
+		for (len2 = strlen(num2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = num2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			x = 1;
+		if (x)
+			_putchar(result[i] + '0');
+	}
+	if (!x)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
 
