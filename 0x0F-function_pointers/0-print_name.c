2dcedaf7036aa2443b15edc389fa66f8682d514d
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-19 04:46:38 -0700

    complete task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 
-/*
+/**
  * print_name - prints a name
  * @name: name to print
  * @f: pointer to function
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
