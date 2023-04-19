424572ebb6f7a769518e02d94f7db4c07322a58c
goonerlabs <aowolabiadewumi@yahoo.com>
2023-04-19 04:51:41 -0700

    complete task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
+
 #endif /* FUNCTION_POINTERS_H */
