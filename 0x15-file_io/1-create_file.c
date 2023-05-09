2c4676dc874ea5a30f88536d636c3d6cffa7af29
goonerlabs <aowolabiadewumi@yahoo.com>
2023-05-08 18:26:31 -0700

    complete task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (fp == -1)
 		return (-1);
 	len = strlen(text_content);
-	if (len == 0)
+	if (len == 0 || text_content == NULL)
 	{
 		close(fp);
 		return (1);
