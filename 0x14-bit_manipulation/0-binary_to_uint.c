7b14e238ca5d4dc748d4b8f4a409b51cfb07bec7
goonerlabs <goonerlabs@gmail.com>
2023-07-07 12:26:54 +0100

    complete task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,26 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to the binary number as a string
- * Return: unsigned int
+ * @b: the binary number to convert (represented as a string)
+ *
+ * Return: the usingned int (decimal number)
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result, j;
+	unsigned int res = 0, j = 1;
 	int i;
 
 	if (b == NULL)
 		return (0);
-	result = 0;
-	i = (int) strlen(b) - 1;
-	j = 1;
+	i = (int)strlen(b) - 1;
 	while (i >= 0)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		result += (b[i] - '0') * j;
+		res += (b[i] - '0') * j;
 		j *= 2;
 		i--;
 	}
-	return (result);
+	return (res);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,11 +7,11 @@
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int mul(unsigned int a, unsigned int b);
+void print_binary(unsigned long int n);
 int get_endianness(void);
-
 #endif
