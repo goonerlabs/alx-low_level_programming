466d0e0d199fe2934de018c7118c59ca83e5f9ef
goonerlabs <goonerlabs@gmail.com>
2023-08-18 17:33:18 +0100

    second attempt at fixing bug in task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp = *h;
+	dlistint_t *new_node = NULL, *temp = *h;
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
@@ -20,9 +20,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->prev = NULL;
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
+		new_node->next = temp;
+		if (temp != NULL)
+			temp->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
