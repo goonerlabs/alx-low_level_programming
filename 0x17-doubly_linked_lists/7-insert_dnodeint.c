0d50dd035bd6976e79d68e309ca49243b9a90c5b
goonerlabs <goonerlabs@gmail.com>
2023-08-18 18:20:07 +0100

    change i to start counting at 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = NULL, *temp = *h;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -35,12 +35,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->prev = temp->prev;
 			temp->prev->next = new_node;
 			temp->prev = new_node;
-			return (new_node);
-		}
-		if (temp->next == NULL && i == idx + 1)
-		{
-			new_node->prev = temp;
-			temp->next = new_node;
 			return (new_node);
 		}
 		temp = temp->next;
