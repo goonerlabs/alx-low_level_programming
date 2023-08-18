f455f51c1569af44d7b51a1e18fd5e303775b906
goonerlabs <goonerlabs@gmail.com>
2023-08-18 19:23:19 +0100

    add check when deleting from index at 0 to avoid derefernce of NULL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,11 +13,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp = *head, *prev = NULL;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
 		*head = temp->next;
+		if (temp->next)
+			(temp->next)->prev = NULL;
 		free(temp);
 		return (1);
 	}
