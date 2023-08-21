6b6f1cc71d1c0e74988ebca3401f2cb3c65c813f
goonerlabs <goonerlabs@gmail.com>
2023-08-21 14:20:38 +0100

    complete task 100

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,64 @@
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference between a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a divided by  b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of a divided by b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
