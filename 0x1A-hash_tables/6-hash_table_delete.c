4adfcf3f49e4f9d0cd316cb7d0e8f66cac07df6f
goonerlabs <goonerlabs@gmail.com>
2024-05-01 19:11:05 +0100

    complete task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - frees a hash table
+ * @ht: pointer to the hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	hash_node_t *tmp1;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+
+	while(i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			tmp1 = tmp;
+			tmp = tmp->next;
+			free(tmp1->key);
+			free(tmp1->value);
+			free(tmp1);
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
